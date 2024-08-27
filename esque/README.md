198ac180988bf5a0eb7b46d2be62fd569faf7e4c
cursedcadaver <collinschacha@hotmail.com>
2024-08-27 18:11:50 +0300

    batman

---
+++ esque/README.md
@@ -0,0 +1,1 @@
+This is a banger plugin that hopes to mimmick some of the cool ipad math functionality and also integrate ai within Obsidian which should hopefully stop me from flip flopping between browsers. 
---
+++ esque/modal.ts
@@ -0,0 +1,39 @@
+import { App, Modal, Setting } from "obsidian";
+
+export class ExampleModal extends Modal {
+	result: string;
+	onSubmit: (result: string) => void;
+
+	constructor(app: App, onSubmit: (result: string) => void) {
+		super(app);
+		this.onSubmit = onSubmit;
+	}
+
+	onOpen() {
+		const { contentEl } = this;
+
+		contentEl.createEl("h1", { text: "Ask mistral any question?" });
+
+		new Setting(contentEl)
+			.setName("Name")
+			.addText((text) =>
+				text.onChange((value) => {
+					this.result = value
+				}));
+
+		new Setting(contentEl)
+			.addButton((btn) =>
+				btn
+					.setButtonText("Submit")
+					.setCta()
+					.onClick(() => {
+						this.close();
+						this.onSubmit(this.result);
+					}));
+	}
+
+	onClose() {
+		let { contentEl } = this;
+		contentEl.empty();
+	}
+}
